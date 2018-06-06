549573e27aa6c0cde17eb887aa2c4d14acb2d4ba
Alex <sasha.pankratiew@gmail.com>
2018-06-06 10:21:49 +0300

    Added building-selection

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import requests
 
 url = "https://students.bsuir.by/api/v1/auditory"
 
+buildID = int(input('Введите номер корпуса: '))
+
+
 response = requests.get(url)
 kek = response.json()
 for mem in kek:
-	print(mem['name'], '-', mem['buildingNumber']['id'])
+	if mem['buildingNumber']['id'] == buildID:
+		print(mem['name'], '-', mem['buildingNumber']['id'])
