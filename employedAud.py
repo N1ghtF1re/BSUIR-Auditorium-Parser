0c8a9395ee323468f95c5840c43b4283b8220500
Alex <sasha.pankratiew@gmail.com>
2018-06-06 19:07:32 +0300

    Now you can look at the busy audience (Thanks to the BSUIR API FOR SPEED)

--- employedAud.py
+++ employedAud.py
@@ -6,21 +6,32 @@ import datetime
 groupsURL = 'https://students.bsuir.by/api/v1/groups'
 
 response = requests.get(groupsURL)
-GroupList = response.json()
+GroupList = response.json() #Список всех групп
 
 scheduleURL='https://students.bsuir.by/api/v1/studentGroup/schedule?studentGroup='
-headers = {"Content-Type": "application/json"}
 
 nowTime= datetime.datetime.now()
 
+employed = set();
+
 for group in GroupList:
-    response = requests.get(scheduleURL+group['name'])
-    schedule = response.json()
-    
-    for lesson in schedule['todaySchedules']:
-        startTime = lesson['startLessonTime'].split(':'); #Начало лекции. 0 элемент - часы кортежа, 1 - минуты
-        endTime = lesson['endLessonTime'].split(':'); # Конец лекции
-        startTime = nowTime.replace(hour=int(startTime[0]), minute = int(startTime[1]))
-        endTime = nowTime.replace(hour=int(endTime[0]), minute = int(endTime[1]))
-        if startTime < nowTime < endTime:
-            print(lesson['auditory'])
+
+    try:
+        response = requests.get(scheduleURL+group['name'])
+        schedule = response.json() # Полное расписание на сегодня
+        if schedule['todaySchedules'] != []:
+            for lesson in schedule['todaySchedules']: # Перебор сегодняшних пар группы
+                startTime = lesson['startLessonTime'].split(':'); #Начало лекции. 0 элемент - часы кортежа, 1 - минуты
+                endTime = lesson['endLessonTime'].split(':'); # Конец лекции
+                startTime = nowTime.replace(hour=int(startTime[0]), minute = int(startTime[1]))
+                endTime = nowTime.replace(hour=int(endTime[0]), minute = int(endTime[1]))
+                print('Loading...')
+                if startTime < nowTime < endTime: # Пара в данный момент идет
+                    for aud in lesson['auditory']:
+                        employed.add(aud);
+    except KeyError: # API БГУИРа самый лучший ♥ (нет)
+        print('')
+    except ValueError:
+        print('')
+
+print(employed)
