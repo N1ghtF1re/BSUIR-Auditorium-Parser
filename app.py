386f474ed38e9cd1804f78eb4a7b6a32fe98566f
Alex <sasha.pankratiew@gmail.com>
2018-06-06 10:40:24 +0300

    Added floor selection

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import requests
 url = "https://students.bsuir.by/api/v1/auditory"
 
 buildID = int(input('Введите номер корпуса: '))
-
+Floor = int(input('Введите этаж( -1, если не имеет значение )'))
 
 response = requests.get(url)
-kek = response.json()
-for mem in kek:
-	if mem['buildingNumber']['id'] == buildID:
-		print(mem['name'], '-', mem['buildingNumber']['id'])
+AudList = response.json()
+
+for aud in AudList:
+	currBuild = aud['buildingNumber']['id'] # Номер корпуса
+	audID = aud['name']; # Номер аудитории
+	if not(str(audID)[0] in {'1','2','3','4','5','6','7','8','9','0'}):
+		continue
+	currFloor = int(str(audID)[0]); # Этаж
+	if (currBuild == buildID) and ((Floor == -1) or (currFloor == Floor)):
+		print(audID, '-', aud['buildingNumber']['id'])
